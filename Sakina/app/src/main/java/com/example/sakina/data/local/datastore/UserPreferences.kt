e38d15f81179898cb780e1739310ba6d1e1e0868
Mariam Magdy <mariammagdy682006@gmail.com>
2026-01-28 19:04:36 +0200

    feat(settings): UI, ViewModel, and DataStore preferences

--- Sakina/app/src/main/java/com/example/sakina/data/local/datastore/UserPreferences.kt
+++ Sakina/app/src/main/java/com/example/sakina/data/local/datastore/UserPreferences.kt
@@ -1,4 +1,20 @@
 package com.example.sakina.data.local.datastore
 
-class UserPreferences {
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
+
+object PreferencesKeys {
+
+    // User info
+    val NAME = stringPreferencesKey("name")
+    val EMAIL = stringPreferencesKey("email")
+    val LOCATION = stringPreferencesKey("location")
+
+    // Notifications
+    val PRAYER_NOTIF = booleanPreferencesKey("prayer_notifications")
+    val AZKAR_NOTIF = booleanPreferencesKey("azkar_notifications")
+
+    // Appearance & language
+    val DARK_MODE = booleanPreferencesKey("dark_mode")
+    val LANGUAGE = stringPreferencesKey("language")
 }
